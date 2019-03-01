127a06f53058f69b358a6cc75d665060d82baf73
Miguel Pignatelli <emepyc@gmail.com>
2019-03-01 01:36:39 +0100

    bugfix: model route loads model component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Gene from './Pages/Gene';
+import Model from './Pages/Model';
 import Home from './Pages/Home';
 import Downloads from './Pages/Downloads';
 import Masthead from './Components/Masthead';
@@ -21,8 +22,9 @@ const App = (props) => (
             <Route exact path='/' component={Home}/>
             <Route exact path='/downloads' component={Downloads}/>
             <Route path='/table' component={Table}/>
+            <Route path='/table' component={Table}/>
             <Route path='/gene/:geneId' component={Gene}/>
-            <Route path='/model/:modelId' component={Gene}/>
+            <Route path='/model/:modelId' component={Model}/>
           </main>
         </div>
         <Footer/>
