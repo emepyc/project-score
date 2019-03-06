6fcbedc9807c577ff68fad9ff3355af3413b80af
Miguel Pignatelli <emepyc@gmail.com>
2019-03-06 21:34:52 +0100

    eliminate api authentication

--- src/api/api.js
+++ src/api/api.js
@@ -3,24 +3,6 @@ import qs from 'query-string';
 
 
 const API_BASEURL = process.env.REACT_APP_API_BASEURL;
-const USERNAME = process.env.REACT_APP_API_USERNAME;
-const PASSWORD = process.env.REACT_APP_API_PASSWORD;
-
-
-function getToken() {
-  return axios({
-    method: 'POST',
-    url: '/login',
-    data: {
-      username: USERNAME,
-      password: PASSWORD,
-    },
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    baseURL: API_BASEURL,
-  });
-}
 
 function paramsSerializer(params) {
   params.agg = params.agg ? JSON.stringify(params.agg) : undefined;
@@ -29,30 +11,18 @@ function paramsSerializer(params) {
 }
 
 export function post(endpoint, data) {
-  return getToken().then(response => {
     return axios.post(
       `${API_BASEURL}/${endpoint}`,
       data,
-      {
-        headers: {
-          Authorization: `Bearer ${response.data.accessToken}`
-        }
-      }
     );
-  });
 }
 
 export function get(endpoint, params = {}) {
-  return getToken().then(response => {
     return axios.get(
       `${API_BASEURL}/${endpoint}`,
       {
         params,
         paramsSerializer,
-        headers: {
-          Authorization: `Bearer ${response.data.accessToken}`
-        }
       }
     );
-  });
 }
