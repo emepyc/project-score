5567c4f8c3284426bfc67b4edb9484c78258ade1
Miguel Pignatelli <emepyc@gmail.com>
2019-03-16 12:11:06 +0100

    Change about text in home page

--- src/Components/HomeProjectDescription/index.js
+++ src/Components/HomeProjectDescription/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import {Row, Col} from 'reactstrap';
 import HomeSectionDescription from '../HomeSectionDescription';
 
 const HomeProjectDescription = () => (
   <React.Fragment>
     <h2 className="display-4">About us</h2>
     <Row>
-      <Col xs={12} sm={4}>
+      <Col xs={12} sm={6}>
         <HomeSectionDescription>
-          We performed genome-scale CRISPR-Cas9 screens in ~200 human cancer
-          cell lines from 12 cancer types
+          We are performing and analysing genome-scale CRISPR-Cas9 screens across a diverse collection of human cancer
+          cell models
         </HomeSectionDescription>
       </Col>
-      <Col xs={12} sm={4}>
+      <Col xs={12} sm={6}>
         <HomeSectionDescription>
-          We identified core fitness genes, providing insights into important
-          cellular processes
-        </HomeSectionDescription>
-      </Col>
-      <Col xs={12} sm={4}>
-        <HomeSectionDescription>
-          We identified fitness genes is specific sub-groups of cancer cell
-          lines
+          We have identified dependencies operative in cancer cells which provides insights into basic cancer biology
+          and which can be utilised to develop new and more effective targeted therapies for patients.
         </HomeSectionDescription>
       </Col>
     </Row>
