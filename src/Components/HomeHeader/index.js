2ceacaba3105c08c1ce33de0f0d017db61c7e1ec
Miguel Pignatelli <emepyc@gmail.com>
2019-04-10 00:54:07 +0200

    improvements in donut plot (hiding it on mobile)

--- src/Components/HomeHeader/index.js
+++ src/Components/HomeHeader/index.js
@@ -11,7 +11,7 @@ const header = () => {
         className="animated fadeInUp text-center"
         style={{marginTop: '60px', marginBottom: '80px'}}
       >
-        <object type='image/svg+xml' data={logo} style={{width: '80%'}}/>
+        <img src={logo} style={{width: '80%'}} />
       </div>
       <p className="lead">Genetic screens to identify cancer dependencies</p>
       <p className="my-2" style={{textAlign: 'center'}}>
--- src/Components/SearchExamples/index.js
+++ src/Components/SearchExamples/index.js
@@ -5,16 +5,19 @@ import "./searchExamples.scss";
 function SearchExamples() {
   return (
     <div>
-      <p className="intro-search-examples">
+      <div className="intro-search-examples">
         Try:
         <Link to={'/gene/SIDG02491'}>BRAF</Link>
-        <Link to={'/gene/SIDG26200'}>PTEN</Link>
-        <Link to={'/model/SIDM01197?scoreMax=0'}>SNU-C1</Link>
+        <Link className='d-none d-md-inline-block' to={'/gene/SIDG26200'}>PTEN</Link>
+        <Link to={'/model/SIDM01197?scoreMax=0'}>SNU&#8209;C1</Link>
         <Link to={'/table?tissue=Breast'}>Breast</Link>
-        <span style={{marginLeft: '20px'}}>
-                Or:<Link to={'/table'}>explore all the data</Link>
-              </span>
-      </p>
+        <span className='d-none d-md-inline-block' style={{marginLeft: '20px'}}>
+          Or:<Link to={'/table'}>explore all the data</Link>
+        </span>
+        <div className='d-md-none'>
+          Or:<Link to={'/table'}>explore all the data</Link>
+        </div>
+      </div>
     </div>
   )
 }
