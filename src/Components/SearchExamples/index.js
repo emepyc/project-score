e7c85ca43da46a84dd7a07c07abdb24e9c52b5ce
Miguel Pignatelli <emepyc@gmail.com>
2019-06-27 23:51:43 +0200

    excludePanCancerGenes set to 1 by default

--- src/Components/SearchExamples/index.js
+++ src/Components/SearchExamples/index.js
@@ -9,7 +9,7 @@ function SearchExamples() {
         Try:
         <Link to={'/gene/SIDG02491'}>BRAF</Link>
         <Link className='d-none d-md-inline-block' to={'/gene/SIDG26200'}>PTEN</Link>
-        <Link to={'/model/SIDM01197?scoreMax=0&excludePanCancerGenes=1'}>SNU&#8209;C1</Link>
+        <Link to={'/model/SIDM01197?scoreMax=0'}>SNU&#8209;C1</Link>
         <Link to={'/table?tissue=Breast'}>Breast</Link>
         <span className='d-none d-md-inline-block' style={{marginLeft: '20px'}}>
           Or:<Link to={'/table'}>explore all the data</Link>
