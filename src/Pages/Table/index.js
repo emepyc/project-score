9fe4ff6384905723b84a62327630d3083cce2bcc
Miguel Pignatelli <emepyc@gmail.com>
2019-12-16 15:42:03 +0100

    showing tabs in table page

--- src/Pages/Table/index.js
+++ src/Pages/Table/index.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import {TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
+import classNames from 'classnames';
 
 import TableComponent from '../../Components/Table';
 import CancerTypeFilter from '../../Components/AnalysisFilter';
@@ -9,7 +11,17 @@ import PageHeader from '../../Components/PageHeader';
 import PanCangerGeneFilter from "../../Components/PanCancerGeneFilter";
 import PriorityScoresSection from "../../Components/PriorityScoresSection";
 
+import './table.scss';
+
 function Table() {
+  const [activeTab, setActiveTab] = useState('fitness');
+
+  const tabToggle = tabName => {
+    if (activeTab !== tabName) {
+      setActiveTab(tabName);
+    }
+  };
+
   return (
     <div>
       <Row className='my-3'>
@@ -18,27 +30,76 @@ function Table() {
             header='Explore all data'
           />
         </Col>
-        <Col xs={{size: 12}} md={{size: 8}} lg={{size: 6, offset: 2}}>
-          <Filters/>
-        </Col>
       </Row>
-      <Row className='my-3'>
-        <Col>
-          <PriorityScoresSection/>
-        </Col>
-      </Row>
-      <Row className='my-3'>
-        <Col>
-          <FitnessTable/>
-        </Col>
-      </Row>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            tabindex={0}
+            className={classNames('tabLabel', {active: activeTab === 'fitness'})}
+            onClick={() => tabToggle('fitness')}
+          >
+            Fitness scores
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            tabindex={1}
+            className={classNames('tabLabel', {active: activeTab === 'priorityScores'})}
+            onClick={() => tabToggle('priorityScores')}
+          >
+            Priority scores
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId='fitness'>
+          <Row className='my-3'>
+            <Col xs={{size: 12}} lg={{size: 12, offset: 0}} xl={{size: 6, offset: 6}}>
+              <FitnessFilters/>
+            </Col>
+          </Row>
+          <Row className='my-3'>
+            <Col>
+              <FitnessTable/>
+            </Col>
+          </Row>
+        </TabPane>
+
+        <TabPane tabId='priorityScores'>
+          <Row className='my-3'>
+            <Col xs={{size: 12}} xl={{size: 4, offset: 8}}>
+              <PriorityScoresFilters/>
+            </Col>
+          </Row>
+          <Row className='my-3'>
+            <PriorityScoresSection/>
+          </Row>
+        </TabPane>
+      </TabContent>
     </div>
   )
 }
 
 export default Table;
 
-function Filters() {
+function PriorityScoresFilters() {
+  return (
+    <Card>
+      <CardHeader>
+        Filters
+      </CardHeader>
+      <CardBody>
+        <Row>
+          <Col>
+            <CancerTypeFilter/>
+          </Col>
+        </Row>
+      </CardBody>
+    </Card>
+  );
+}
+
+function FitnessFilters() {
   return (
     <Card>
       <CardHeader>
