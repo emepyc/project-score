d2736afe81931bfd2a92687588829d7afb1b6c9c
Miguel Pignatelli <emepyc@gmail.com>
2020-06-06 21:05:48 +0200

    priority scores card even when no priority score for gene

--- src/Components/GenePriorityScore/index.js
+++ src/Components/GenePriorityScore/index.js
@@ -24,78 +24,80 @@ function GenePriorityScore(props) {
 
   return (
     <div ref={container}>
-      <FetchData
-        endpoint={fetchGenePriorityScore}
-        params={{geneId}}
-        deps={[geneId]}
-      >
-        {priorityScore => {
-          if (priorityScore.length === 0) {
-            return (
-              <div>
-                No priority scores for gene
-              </div>
-            );
-          }
-          return (
-            <React.Fragment>
-              <Card>
-                <CardHeader>
-                  Priority Scores
-                  <span className="help" onClick={modalToggle}>?</span>
-                </CardHeader>
-                <CardBody>
-                  <PriorityScoreForAnalyses width={containerWidth} scores={priorityScore}/>
-                </CardBody>
-              </Card>
-              <Modal isOpen={modalIsOpen} toggle={modalToggle}>
-                <ModalHeader>Priority scores</ModalHeader>
-                <ModalBody>
-                  <div className='priorityScoreHelp'>
-                    <div className='section'>
-                      <h6>Level 1: Biomarker & tumour prevalence</h6>
-                      <PriorityScoresHelpItem label='Class A marker' description={priorityScoresHelp.classAmarker}/>
-                      <PriorityScoresHelpItem label='Class B marker' description={priorityScoresHelp.classBmarker}/>
-                      <PriorityScoresHelpItem label='Class C marker' description={priorityScoresHelp.classCmarker}/>
-                      <PriorityScoresHelpItem label='Weaker marker' description={priorityScoresHelp.weakerMarker}/>
-                      <PriorityScoresHelpItem
-                        label='Mutated in primary tumours'
-                        description={priorityScoresHelp.mutatedInPrimaryTumors}
-                      />
-                    </div>
+      <Card>
+        <CardHeader>
+          Priority Scores
+          <span className="help" onClick={modalToggle}>?</span>
+        </CardHeader>
+        <CardBody>
 
-                    <div className='section'>
-                      <h6>Level 2: Cell lines fitness effect (averaged for sensitive cell lines)</h6>
-                      <PriorityScoresHelpItem
-                        label='Fitness score < -1'
-                        description={priorityScoresHelp.fitnessScoreFold1}
-                      />
-                      <PriorityScoresHelpItem
-                        label='Fitness score < -2'
-                        description={priorityScoresHelp.fitnessScoreFold2}
-                      />
-                      <PriorityScoresHelpItem
-                        label='Fitness score < -3'
-                        description={priorityScoresHelp.fitnessScoreFold3}
-                      />
-                      <PriorityScoresHelpItem label='MAGeCK < 10% FRD' description={priorityScoresHelp.mageck10fdr}/>
-                      <PriorityScoresHelpItem label='MAGeCK < 5% FRD' description={priorityScoresHelp.mageck5fdr}/>
-                      <PriorityScoresHelpItem label='Mutated' description={priorityScoresHelp.mutated}/>
-                      <PriorityScoresHelpItem
-                        label='Enriched pathway'
-                        description={priorityScoresHelp.enrichedPathway}
-                      />
-                    </div>
+          <FetchData
+            endpoint={fetchGenePriorityScore}
+            params={{geneId}}
+            deps={[geneId]}
+          >
+            {priorityScore => {
+              if (priorityScore.length === 0) {
+                return (
+                  <div className="text-center">
+                    No priority scores for gene
                   </div>
-                </ModalBody>
-                <ModalFooter>
-                  <Button onClick={modalToggle}>Ok</Button>
-                </ModalFooter>
-              </Modal>
-            </React.Fragment>
-          )
-        }}
-      </FetchData>
+                );
+              }
+              return (
+                <React.Fragment>
+                  <PriorityScoreForAnalyses width={containerWidth} scores={priorityScore}/>
+                  <Modal isOpen={modalIsOpen} toggle={modalToggle}>
+                    <ModalHeader>Priority scores</ModalHeader>
+                    <ModalBody>
+                      <div className='priorityScoreHelp'>
+                        <div className='section'>
+                          <h6>Level 1: Biomarker & tumour prevalence</h6>
+                          <PriorityScoresHelpItem label='Class A marker' description={priorityScoresHelp.classAmarker}/>
+                          <PriorityScoresHelpItem label='Class B marker' description={priorityScoresHelp.classBmarker}/>
+                          <PriorityScoresHelpItem label='Class C marker' description={priorityScoresHelp.classCmarker}/>
+                          <PriorityScoresHelpItem label='Weaker marker' description={priorityScoresHelp.weakerMarker}/>
+                          <PriorityScoresHelpItem
+                            label='Mutated in primary tumours'
+                            description={priorityScoresHelp.mutatedInPrimaryTumors}
+                          />
+                        </div>
+
+                        <div className='section'>
+                          <h6>Level 2: Cell lines fitness effect (averaged for sensitive cell lines)</h6>
+                          <PriorityScoresHelpItem
+                            label='Fitness score < -1'
+                            description={priorityScoresHelp.fitnessScoreFold1}
+                          />
+                          <PriorityScoresHelpItem
+                            label='Fitness score < -2'
+                            description={priorityScoresHelp.fitnessScoreFold2}
+                          />
+                          <PriorityScoresHelpItem
+                            label='Fitness score < -3'
+                            description={priorityScoresHelp.fitnessScoreFold3}
+                          />
+                          <PriorityScoresHelpItem label='MAGeCK < 10% FRD'
+                                                  description={priorityScoresHelp.mageck10fdr}/>
+                          <PriorityScoresHelpItem label='MAGeCK < 5% FRD' description={priorityScoresHelp.mageck5fdr}/>
+                          <PriorityScoresHelpItem label='Mutated' description={priorityScoresHelp.mutated}/>
+                          <PriorityScoresHelpItem
+                            label='Enriched pathway'
+                            description={priorityScoresHelp.enrichedPathway}
+                          />
+                        </div>
+                      </div>
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button onClick={modalToggle}>Ok</Button>
+                    </ModalFooter>
+                  </Modal>
+                </React.Fragment>
+              )
+            }}
+          </FetchData>
+        </CardBody>
+      </Card>
     </div>
   );
 }
