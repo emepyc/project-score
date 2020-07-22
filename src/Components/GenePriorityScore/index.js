bc118cfd38a649201410963ae13589f73d450883
Miguel Pignatelli <emepyc@gmail.com>
2020-07-22 22:55:44 +0200

    updated tooltip for Target priority score section on gene page

--- src/Components/GenePriorityScore/index.js
+++ src/Components/GenePriorityScore/index.js
@@ -1,7 +1,17 @@
 import React, {useState, useRef} from 'react';
 import {Text} from '@vx/text';
 import {withRouter} from 'react-router-dom';
-import {Button, Card, CardHeader, CardBody, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Tooltip as ReactStrapTooltip
+} from 'reactstrap';
 import * as d3 from 'd3';
 
 import {cancerTypeColor, textDefaultColor} from "../../colors";
@@ -16,9 +26,7 @@ import "./GenePriorityScore.scss";
 
 function GenePriorityScore(props) {
   const [{geneId}] = useUrlParams(props);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const modalToggle = () => setModalIsOpen(!modalIsOpen);
+  const [targetPriorityScoreTooltip, setTargetPriorityScoreTooltip] = useState(false);
 
   const container = useRef(null);
   const containerWidth = useWidth(container);
@@ -27,8 +35,14 @@ function GenePriorityScore(props) {
     <div ref={container}>
       <Card>
         <CardHeader>
-          Target Priority Score
-          <span className="help" onClick={modalToggle}>?</span>
+          Target priority score
+          <sup
+            className='helpAnchor'
+            onMouseEnter={() => setTargetPriorityScoreTooltip(true)}
+            id='target-priority-score-tooltip'
+          >
+            ?
+          </sup>
         </CardHeader>
         <CardBody>
 
@@ -54,74 +68,31 @@ function GenePriorityScore(props) {
                 );
               }
               return (
-                <React.Fragment>
-                  <PriorityScoreForAnalyses width={containerWidth} scores={priorityScore}/>
-                  <Modal isOpen={modalIsOpen} toggle={modalToggle}>
-                    <ModalHeader>Target priority scores</ModalHeader>
-                    <ModalBody>
-                      <div className='priorityScoreHelp'>
-                        <div className='section'>
-                          <h6>Level 1: Biomarker & tumour prevalence</h6>
-                          <PriorityScoresHelpItem label='Class A marker' description={priorityScoresHelp.classAmarker}/>
-                          <PriorityScoresHelpItem label='Class B marker' description={priorityScoresHelp.classBmarker}/>
-                          <PriorityScoresHelpItem label='Class C marker' description={priorityScoresHelp.classCmarker}/>
-                          <PriorityScoresHelpItem label='Weaker marker' description={priorityScoresHelp.weakerMarker}/>
-                          <PriorityScoresHelpItem
-                            label='Mutated in primary tumours'
-                            description={priorityScoresHelp.mutatedInPrimaryTumors}
-                          />
-                        </div>
-
-                        <div className='section mt-5'>
-                          <h6>Level 2: Cell lines fitness effect (averaged for sensitive cell lines)</h6>
-                          <PriorityScoresHelpItem
-                            label='Fitness score < -1'
-                            description={priorityScoresHelp.fitnessScoreFold1}
-                          />
-                          <PriorityScoresHelpItem
-                            label='Fitness score < -2'
-                            description={priorityScoresHelp.fitnessScoreFold2}
-                          />
-                          <PriorityScoresHelpItem
-                            label='Fitness score < -3'
-                            description={priorityScoresHelp.fitnessScoreFold3}
-                          />
-                          <PriorityScoresHelpItem label='MAGeCK < 10% FRD'
-                                                  description={priorityScoresHelp.mageck10fdr}/>
-                          <PriorityScoresHelpItem label='MAGeCK < 5% FRD' description={priorityScoresHelp.mageck5fdr}/>
-                          <PriorityScoresHelpItem label='Mutated' description={priorityScoresHelp.mutated}/>
-                          <PriorityScoresHelpItem
-                            label='Enriched pathway'
-                            description={priorityScoresHelp.enrichedPathway}
-                          />
-                        </div>
-                      </div>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button onClick={modalToggle}>Ok</Button>
-                    </ModalFooter>
-                  </Modal>
-                </React.Fragment>
+                <PriorityScoreForAnalyses width={containerWidth} scores={priorityScore}/>
               )
             }}
           </FetchData>
         </CardBody>
       </Card>
+
+      <ReactStrapTooltip
+        trigger='hover'
+        toggle={() => setTargetPriorityScoreTooltip(false)}
+        placement='top'
+        isOpen={targetPriorityScoreTooltip}
+        target='target-priority-score-tooltip'
+        className='helpAnchor'
+        innerClassName='project-score-tooltip'
+      >
+        {priorityScoresHelp.targetPriorityScores}.
+      </ReactStrapTooltip>
+
     </div>
   );
 }
 
 export default withRouter(GenePriorityScore);
 
-
-function PriorityScoresHelpItem({label, description}) {
-  return (
-    <div className='item'>
-      <b>{label}</b>:{' '}
-      <span dangerouslySetInnerHTML={{__html: description}}/>
-    </div>
-  );
-}
 
 function getLabelFontSize(labelLength, maxLabelWidth) {
   let fontFactor = 8, fontSize = 0.9;
@@ -193,7 +164,6 @@ function PriorityScoreForAnalyses({width, scores}) {
             cellWidth={cellWidth}
             blockMargin={blockMargin}
             onHelp={(label, description) => {
-              console.log(label, description);
               setHelpModal({label, description});
               helpModalToggle();
             }}
