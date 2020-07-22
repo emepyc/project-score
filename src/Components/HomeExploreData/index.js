83c39ffde299cb92c1e9741f6fe3ed3207142449
Miguel Pignatelli <emepyc@gmail.com>
2020-07-22 22:24:48 +0200

    changed Explore data text in home page

--- src/Components/HomeExploreData/index.js
+++ src/Components/HomeExploreData/index.js
@@ -11,12 +11,7 @@ const HomeExploreData = () => (
         <img src={targetLogo} width={50} style={{float: 'left', margin: '0 10px'}}/>
         <h5>Fitness scores</h5>
         <div>
-          Fitness scores are a quantitative measure of the reduction of cell viability elicited by a
-          gene inactivation, via CRISPR-Cas9
-          targeting. This is based on Bayes Factor value computed using BAGEL starting from CRISPRcleanR corrected
-          gene
-          depletion fold changes, and scaled to a 5% false discovery rate threshold (from classifying reference
-          essential/non-essential genes based on BF rankings)
+          Investigate the effect on cell line viability elicited by CRISPR-Cas9 mediated gene activation.
         </div>
         <div className='my-2 font-weight-bold'>
           <Link to='/table'>
@@ -29,9 +24,7 @@ const HomeExploreData = () => (
         <img src={targetLogo} width={50} style={{float: 'left', margin: '0 10px'}}/>
         <h5>Target priority scores</h5>
         <div>
-          Target priority scores are a quantitative assessment of the potential of each gene as a candidate cancer therapeutic target. It
-              is computed by an analytical framework that combines CRISPR knockout gene fitness effects with
-              biomarker and patient data to output a target priority score from 0 – 100 (highest to lowest)
+          Investigate candidate therapeutic target in different cancer types.
         </div>
         <div className='my-2 font-weight-bold'>
           <Link to='/table?dataTab=priorityScores'>
