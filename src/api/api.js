1c4ee216f40de987ea578d2f3747792bcd447406
Miguel Pignatelli <emepyc@gmail.com>
2021-03-06 13:09:13 +0100

    api authentication restored

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 import qs from 'query-string';
 
 const API_BASEURL = process.env.REACT_APP_API_BASEURL;
+const USERNAME = process.env.REACT_APP_API_USERNAME;
+const PASSWORD = process.env.REACT_APP_API_PASSWORD;
 
 function paramsSerializer(params) {
   const serialisedParams = {
@@ -11,13 +13,32 @@ function paramsSerializer(params) {
   return qs.stringify(serialisedParams, {depth: 0});
 }
 
+async function getToken(signal) {
+  const response = await fetch(`${API_BASEURL}/login`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // add signal?
+    signal,
+    body: JSON.stringify({
+      username: USERNAME,
+      password: PASSWORD,
+    }),
+  });
+  return await response.json();
+}
+
 export async function get(endpoint, params = {}, signal) {
   const serialisedParams = paramsSerializer(params);
+  const token = await getToken(signal);
   const result = await fetch(`${API_BASEURL}/${endpoint}?${serialisedParams}`, {
     method: 'GET',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token.accessToken}`,
     },
     signal,
   }).catch(err => {
